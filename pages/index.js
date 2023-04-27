1307dfadde2aa8e6c4368e4820904aef712b11c0
Hazel <119948286+Hzlgrsly@users.noreply.github.com>
2023-04-27 18:05:26 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hzlgrsly👽</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
